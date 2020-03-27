2453d8e72b0356e6013c204ebe13fddb980fa5df
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-03-27 21:19:10 +0100

    - work in progress

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+import {HomePage, CartPage} from '../pages'
 import './app.css'
-import {withBookstoreService} from '../hoc'
 
-const App = ({bookstoreService}) => {
-    console.log(bookstoreService.getBooks());
-    return <div>App</div>
+const App = () => {
+    return (
+        <Switch>
+            <Route
+                path="/"
+                component={HomePage}
+                exact/>
+            <Route
+                path="/cart"
+                component={CartPage}
+                />
+        </Switch>
+    )
 };
 
-export default withBookstoreService()(App);
+export default App;
---
+++ src/components/book-list-item/index.js
@@ -0,0 +1,1 @@
+import BookListItem from "./book-list-item";
