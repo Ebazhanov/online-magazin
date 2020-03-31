c71a6388306f0a541a231d93d8ade367ef6d7292
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-04-01 01:09:55 +0200

    - work in progress

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,9 +1,22 @@
 import React, {Component} from "react";
 import BookListItem from "../book-list-item/book-list-item";
 import './book-list.css'
+import {bindActionCreators} from "redux";
+import {booksLoaded} from "../../actions";
 import {connect} from 'react-redux'
+import withBookstoreService from "../hoc/with-bookstore-service";
 
 class BookList extends Component {
+
+    componentDidMount() {
+        // 1. receive data
+        const {bookstoreService} = this.props;
+        const data = bookstoreService.getBooks();
+
+        // 2. dispatch action to store
+        this.props.booksLoaded(data)
+    }
+
     render() {
         const {books} = this.props;
         return (
@@ -24,7 +37,12 @@ class BookList extends Component {
 const mapStateToProps = ({books}) => {
     return {
         books
-    }
+    };
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = {
+    booksLoaded
+};
+
+export default withBookstoreService()(
+    connect(mapStateToProps, mapDispatchToProps)(BookList));
